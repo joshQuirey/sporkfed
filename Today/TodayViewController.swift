2ada9b2e48821d556c5d6622d6ad4036b0e03159
jquirey <jquirey@flandersinc.com>
2019-10-16 13:07:14 -0500

    Gets todays meal and displays the image and name

--- Today/TodayViewController.swift
+++ Today/TodayViewController.swift
@@ -8,16 +8,32 @@
 
 import UIKit
 import NotificationCenter
+import CoreData
 
 class TodayViewController: UIViewController, NCWidgetProviding {
         
-    @IBOutlet weak var planDate: UILabel!
-    @IBOutlet weak var planMonth: UILabel!
-    @IBOutlet weak var planDay: UILabel!
+    var today: [PlannedDay] = []
+    let manager = CoreDataManager.init(modelName: "MealModel")
+    var managedObjectContext: NSManagedObjectContext?
+    
+    @IBOutlet weak var mealImage: UIImageView!
+    @IBOutlet weak var mealName: UILabel!
+    
+    @IBAction func AddMeal(_ sender: Any) {
+    }
+    
+    
+    @IBAction func ViewMenu(_ sender: Any) {
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.managedObjectContext = manager.managedObjectContext
+        //self.today =
+        print("before")
+        fetchPlans()
+        print("after")
     }
         
     func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
@@ -30,29 +46,48 @@ class TodayViewController: UIViewController, NCWidgetProviding {
         completionHandler(NCUpdateResult.newData)
     }
     
-}
-
-class PlanTableViewCell: UITableViewCell {
-    /////////////////////////////
-    //Outlets
-    /////////////////////////////
-    @IBOutlet weak var planDate: UILabel!
-    @IBOutlet weak var planMonth: UILabel!
-    @IBOutlet weak var planDay: UILabel!
-    @IBOutlet weak var mealName: UILabel!
-    @IBOutlet weak var mealImage: UIImageView!
-    @IBOutlet weak var prep: UILabel!
-    @IBOutlet weak var cook: UILabel!
-    @IBOutlet weak var mealCategories: UILabel!
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-    
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+    private func fetchPlans() {
+        //dateFrom and dateTo
+        //var date = Date()
+        var calendar = Calendar.current
+        calendar.timeZone = NSTimeZone.local
         
-        // Configure the view for the selected state
+        let dateFrom = calendar.startOfDay(for: Date())
+        let dateTo = calendar.date(byAdding: .day, value: 1, to: dateFrom)
+        print(dateFrom)
+        print(dateTo)
+        let fromPredicate = NSPredicate(format: "date >= %@", dateFrom as NSDate)
+        let toPredicate = NSPredicate(format: "date < %@", dateTo! as NSDate)
+        let datePredicate = NSCompoundPredicate(andPredicateWithSubpredicates: [fromPredicate,toPredicate])
+        
+        
+        // Create Fetch Request
+        let fetchRequest: NSFetchRequest<PlannedDay> = PlannedDay.fetchRequest()
+        
+        // Configure Fetch Request
+        fetchRequest.predicate = datePredicate //NSPredicate(format: "isCompleted == nil")
+        
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: #keyPath(PlannedDay.date), ascending: true)]
+        
+        // Perform Fetch Request
+        self.managedObjectContext!.performAndWait {
+            do {
+                // Execute Fetch Request
+                today = try fetchRequest.execute()
+                print(today)
+
+                //Reload Table View
+                if (today.count > 0) {
+                    mealName.text = today[0].meal?.mealName
+                    mealImage.image = UIImage(data: today[0].meal!.mealImage!)
+//                imageButton.setBackgroundImage(UIImage(data: meal!.mealImage!), for: .normal)
+                }
+            } catch {
+                let fetchError = error as NSError
+                print("Unable to Execute Fetch Request")
+                print("\(fetchError), \(fetchError.localizedDescription)")
+            }
+        }
     }
+    
 }
