0173b983f064d49ffb9d80e37494dd374c4cff52
jquirey <jquirey@flandersinc.com>
2020-02-06 13:41:09 -0600

    step by step

--- WhatsForDinner/CoreDataManager.swift
+++ WhatsForDinner/CoreDataManager.swift
@@ -19,22 +19,87 @@ class CoreDataManager {
     
     static var persistentContainer: NSPersistentContainer = {
         
-        //let container = NSPersistentCloudKitContainer(name: "MealModel")
-        let container = NSPersistentContainer(name: "MealModel")
+        let container = NSPersistentCloudKitContainer(name: "MealModel")
+        //let container = NSPersistentContainer(name: "MealModel")
         
-//        do {
-//            try container.initializeCloudKitSchema()
-//        } catch {
-//
-//        }
+        do {
+            try container.initializeCloudKitSchema()
+        } catch {
+
+        }
+        
+        var options = [AnyHashable : Any]()
+               options[NSMigratePersistentStoresAutomaticallyOption] = true
+               options[NSInferMappingModelAutomaticallyOption] = true
+        
+        var previousStoreURL = previousLocalPersistentStoreURL
+        print("Old \(previousStoreURL)")
+        var previousStore = container.persistentStoreCoordinator.persistentStore(for: previousStoreURL)
+        print("Old Store \(previousStore)")
+        do {
+            try container.persistentStoreCoordinator.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: previousStoreURL, options: options)
+                
+        } catch {
+            let addPersistentStoreError = error as NSError
+            print("Unable to Add Persistent Store")
+            print("\(addPersistentStoreError.localizedDescription)")
+        }
+        previousStore = container.persistentStoreCoordinator.persistentStore(for: previousStoreURL)
+        print("Old Store \(previousStore)")
+        
+        //var newStoreURL: URL
         
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-                if let error = error as NSError? {
-                    fatalError("Unresolved error \(error), \(error.userInfo)")
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+                
+            var newStoreURL = storeDescription.url!
+            print("New \(newStoreURL)")
+            
+            print("test test test")
+            print(container.persistentStoreCoordinator.persistentStores)
+            print("end test end test end test")
+    
+   
+            if (previousStore != nil) {
+                do {
+                    try container.persistentStoreCoordinator.migratePersistentStore(previousStore!, to: newStoreURL, options: options, withType: NSSQLiteStoreType)
+                    
+                } catch let error {
+                        print("migrate failed with error : \(error)")
                 }
-            })
-            return container
+                
+                do {
+                    try container.persistentStoreCoordinator.destroyPersistentStore(at: previousStoreURL, ofType: NSSQLiteStoreType, options: options)
+
+                } catch let error {
+                        print("destroy failed with error : \(error)")
+                }
+            }
+            
+            print("test test test")
+            print(container.persistentStoreCoordinator.persistentStores)
+            print("end test end test end test")
+            
+            
+        })
+        
+        
+        
+        return container
     }()
+    
+    static var previousLocalPersistentStoreURL: URL {
+        //Helpers
+        let storeName = "MealModel.sqlite"
+        let fileManager = FileManager.default
+    
+        //new
+        let documentsDirectoryURL = fileManager.containerURL(forSecurityApplicationGroupIdentifier:"group.co.app41.sporkfed")
+        
+        return documentsDirectoryURL!.appendingPathComponent(storeName)
+    }
     
     func migrateLocalToCloud() {
 
